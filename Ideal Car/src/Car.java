f3c4d910f7bf41e3ee8322a8b0a06dd3c312e5e6
jonathan.aragon <jonathan.aragon@MU-C1-15.mullen.pvt.k12.co.us>
2021-09-21 11:56:45 -0600

    Initial Commit

---
+++ Ideal Car/src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car
+	{
+int weight;
+int horsepower;
+int wheelbase;
+double turningRadius;
+String color;
+		public Car(int w, int h, int wB, double tR, String c)
+			{
+
+			}
+	}
